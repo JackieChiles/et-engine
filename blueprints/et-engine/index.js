5af3669598085e8dbfddabbcd871c840668929e9
Drew Gaynor <drew.gaynor@inin.com>
2016-02-26 11:49:55 -0500

    Attempted fix for CLI error.

--- blueprints/et-engine/index.js
+++ blueprints/et-engine/index.js
@@ -1,4 +1,5 @@
 module.exports = {
+    normalizeEntityName: function () {},
     afterInstall: function () {
         console.log('In afterInstall');
         return this.addPackagesToProject([{ name: 'git+ssh@github.com:JackieChiles/et-addon.git' }]);
