27b708c49f1250b311932c0eeea2e6e2ffa4856f
Drew Gaynor <drew.gaynor@inin.com>
2016-03-10 11:14:35 -0500

    Added addon flag for file watching in consuming app.

--- index.js
+++ index.js
@@ -4,5 +4,8 @@
 var EngineAddon = require('ember-engines/lib/engine-addon');
 
 module.exports = EngineAddon.extend({
-  name: 'et-engine'
+    name: 'et-engine',
+    isDevelopingAddon: function () {
+        return true;
+    }
 });
